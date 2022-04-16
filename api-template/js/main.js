1982bca2acfd72ce5f9471858293c8797d31215d
viola <nuahavyll@gmail.com>
2022-04-16 18:08:39 +0300

    Add files via upload

---
+++ api-template/js/main.js
@@ -0,0 +1,27 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if( data.media_type === 'image' ){
+          document.querySelector('img').src = data.hdurl
+        }else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+        }
+        document.querySelector('.title').innerText = data.title
+        document.querySelector('.date').innerText = data.date
+        document.querySelector('p').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
